635245fd25acdf80b91536bfc77f8d67d41388bd
Matthew Howells <howellsmatthew@hotmail.co.uk>
2025-05-23 15:24:00 +0100

    Add test function for seasonal_gatekeeping
    Add a test function for the seasonal_gatekeeping
    function. Rename ts variable to ts_sample and
    redefine it.

--- src/sd_tests.py
+++ src/sd_tests.py
@@ -10,7 +10,7 @@ sample_stocks = [
     np.array([6000, 5999.67150899, 5999.34303597, 5999.01458093, 5998.68614387]),
 ]
 presenting_rate = 0.002
-ts = np.linspace(0, 365 * 3, 100000 + 1)
+ts_sample = np.array([0, 1, 2, 3, 4])
 
 
 def test_proportional_gatekeeping():
@@ -30,7 +30,7 @@ def test_proportional_gatekeeping():
         stocks=[sample_stocks[0], sample_stocks[1], sample_stocks[2]],
         population=sample_stocks[0] + sample_stocks[1] + sample_stocks[2],
         presenting_rate=presenting_rate,
-        t=ts,
+        t=ts_sample,
     )
     expected_referrals_time_series = [
         np.array([2, 2.0002847, 2.00056941, 2.00085412, 2.00113884]),
@@ -65,12 +65,47 @@ def test_fixed_gatekeeping():
         stocks=[sample_stocks[0], sample_stocks[1], sample_stocks[2]],
         population=sample_stocks[0] + sample_stocks[1] + sample_stocks[2],
         presenting_rate=presenting_rate,
-        t=ts,
+        t=ts_sample,
     )
     expected_referrals_time_series = [
         np.array([2, 2.0002847 , 2.00056941, 2.00085412, 2.00113884]),
         np.array([6, 6.00019708, 6.00039412, 6.00059112, 6.00078809]),
         np.array([8, 7.99951822, 7.99903647, 7.99855475, 7.99807308]),
+    ]
+    assert np.allclose(
+        obtained_referrals_time_series[0], expected_referrals_time_series[0]
+    )
+    assert np.allclose(
+        obtained_referrals_time_series[1], expected_referrals_time_series[1]
+    )
+    assert np.allclose(
+        obtained_referrals_time_series[2], expected_referrals_time_series[2]
+    )
+
+
+def test_seasonal_gatekeeping():
+    gatekeeping = sd.seasonal_gatekeeping(baseline=8, amplitude=4, phase_shift=0)
+    assert callable(gatekeeping)
+
+    obtained_referrals_time_point = gatekeeping(
+        stocks=[sample_stocks[0][0], sample_stocks[1][0], sample_stocks[2][0]],
+        population=sample_stocks[0][0] + sample_stocks[1][0] + sample_stocks[2][0],
+        presenting_rate=presenting_rate,
+        t=0,
+    )
+    expected_referrals_time_point = [2, 6, 0]
+    assert np.allclose(obtained_referrals_time_point, expected_referrals_time_point)
+
+    obtained_referrals_time_series = gatekeeping(
+        stocks=[sample_stocks[0], sample_stocks[1], sample_stocks[2]],
+        population=sample_stocks[0] + sample_stocks[1] + sample_stocks[2],
+        presenting_rate=presenting_rate,
+        t=ts_sample,
+    )
+    expected_referrals_time_series = [
+        np.array([2, 2.0002847 , 2.00056941, 2.00085412, 2.00113884]),
+        np.array([6, 6.00019708, 6.00039412, 6.00059112, 6.00078809]),
+        np.array([0, 0.06837164, 0.13672292, 0.20503342, 0.27328278]),
     ]
     assert np.allclose(
         obtained_referrals_time_series[0], expected_referrals_time_series[0]
